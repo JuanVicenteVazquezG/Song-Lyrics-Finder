465904eadce7eab95c1faba459ed15b6a9b420f8
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-08-15 17:19:55 +0200

    Connect with API and Show lyrics

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Song-Lyrics-Finder
  Repo that helps to find a lyric
+
+[Api] used
+
+[Api]: (https://api.lyrics.ovh/v1/Metallica/Seek%20&%20Destroy)
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // Imports
+import API from './api.js';
 import * as UI from './interface.js';
 
 
@@ -15,7 +16,7 @@ function searchLyrics(event) {
 
     const artist = document.querySelector('#artist').value,
         song = document.querySelector('#song').value;
-
+    // If the user left the fields empty
     if (artist === '' || song === '') {
 
         UI.divMessages.innerHTML = 'Error ... All fields must be filled out';
@@ -24,6 +25,13 @@ function searchLyrics(event) {
             UI.divMessages.innerHTML = '';
             UI.divMessages.classList.remove('error');
         }, 3000);
+    }
+    else {
+        // If the form is fill out make an Api fetch
+        const api = new API(artist, song);
+        api.retrieveDataFromApi()
+            .then(response => { console.log(response.response.lyrics); });
+
     }
 
 } 
